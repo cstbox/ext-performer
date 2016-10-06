3ece8b52ebc379a2497367e18281347fc7428736
Eric Pascual <eric.pascual@cstb.fr>
2016-10-06 18:29:08 +0200

    added test case for config relative path handling

--- test/test_config.py
+++ test/test_config.py
@@ -3,7 +3,9 @@
 
 import unittest
 import os
+import shutil
 
+from pycstbox.config import CONFIG_DIR
 from pycstbox.performer.commons.runner import Runner
 from pycstbox.performer.commons.analytics import PeriodicAnalyzer, AnalyzerError
 
@@ -23,7 +25,7 @@ class ConfigTestCase(unittest.TestCase):
         runner = Runner(config_path=cfg_path, period=PeriodicAnalyzer.PERIOD_DAY)
         with self.assertRaises(AnalyzerError) as cm:
             runner.prepare_analyzers()
-        self.assertTrue('unresolved definition' in cm.exception.message)
+        self.assertIn('unresolved definition', cm.exception.message)
 
     def test_02_mixed(self):
         cfg_path = os.path.join(__here__, 'fixtures/analytics-mixed.cfg')
@@ -39,7 +41,7 @@ class ConfigTestCase(unittest.TestCase):
         runner = Runner(config_path=cfg_path, period=PeriodicAnalyzer.PERIOD_DAY)
         with self.assertRaises(AnalyzerError) as cm:
             runner.prepare_analyzers()
-        self.assertTrue('analyzers_module not configured' in cm.exception.message)
+        self.assertIn('analyzers_module not configured', cm.exception.message)
 
     def test_04_skip(self):
         cfg_path = os.path.join(__here__, 'fixtures/analytics-skip.cfg')
@@ -47,6 +49,37 @@ class ConfigTestCase(unittest.TestCase):
         analyzers = runner.prepare_analyzers()
         self.assertEquals(len(analyzers), 2)
         self.assertSetEqual({a[0].__name__ for a in analyzers}, {'WU1', 'WU3'})
+
+
+class ConfigRelativePathTestCase(unittest.TestCase):
+    import_name = 'wu.cfg'
+    cfg_name = 'analytics-mixed.cfg'
+
+    def setUp(self):
+        self._clean_cfg_files()
+
+    def tearDown(self):
+        self._clean_cfg_files()
+
+    def _clean_cfg_files(self):
+        for name in (self.import_name, self.cfg_name):
+            os.remove(os.path.join(CONFIG_DIR, name))
+
+    def test_01(self):
+        runner = Runner(config_path=self.cfg_name, period=PeriodicAnalyzer.PERIOD_DAY)
+        with self.assertRaises(AnalyzerError) as cm:
+            runner.prepare_analyzers()
+        self.assertIn('No such file or directory', cm.exception.message)
+
+        shutil.copy(os.path.join(__here__, 'fixtures', self.cfg_name), CONFIG_DIR)
+        runner = Runner(config_path=self.cfg_name, period=PeriodicAnalyzer.PERIOD_DAY)
+        with self.assertRaises(AnalyzerError) as cm:
+            runner.prepare_analyzers()
+        self.assertIn('imported configuration file not found', cm.exception.message)
+
+        shutil.copy(os.path.join(__here__, 'fixtures', self.import_name), CONFIG_DIR)
+        runner = Runner(config_path=self.cfg_name, period=PeriodicAnalyzer.PERIOD_DAY)
+        self.assertEquals(len(runner.prepare_analyzers()), 2)
 
 
 if __name__ == '__main__':
